4dc8afa63dbf4d9b0631b6cdf9bbabea68377e32
Charles Randolph <charles.xrandolph2@gmail.com>
2018-09-29 19:34:24 +0200

    Removed comment in README

--- dominos.go
+++ dominos.go
@@ -11,27 +11,6 @@ import (
 	"strconv"
 )
 
-/* Using the Pizza-Voucher-Explorer(Netherlands).
- *
- * There are never any fucking codes when you need them. I've had enough. 
- * From now on, the Dominos-Pizza-Cracker™ will do the job of finding 
- * me those juicy dominos codes automatically. 
- *
- * To use this tool, you're going to need to know
- * 1) The city you reside in (shouldn't be hard).
- * 2) The number of the Dominos store you want to order from.
- * 3) The ASP.Net Session ID of an open connection you have to Dominos.
- *
- * Number 3 is the most confusing to explain. You basically need to open
- * a browser, navigate to a dominos store session, open the web-developer,
- * and then check the outgoing network requests when you submit a voucher.
- * What you want to do is visit the Cookies of the request. In it you can
- * find the ASP.NET_SessionId. That will be your session. The store number
- * and city are also listed in the cookies. 
- *
- * NOTE: You may get your device banned from accessing dominos-store (I did).
-*/
-
 // Type describing the response to a code inquiry.
 type Code struct {
 	Url 				string
